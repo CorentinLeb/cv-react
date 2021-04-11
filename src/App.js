02b14bf0ec3cdf1ef24e2452a031230f2a5641a3
Corentin LE BERRE <leberre.corentin@gmail.com>
2021-04-11 17:58:00 +0200

    defining routes / switch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import Portfolio from './pages/Contact';
+import Knowledges from './pages/Knowledges';
+import NotFound from './pages/NotFound';
+import Contact from './pages/Contact'
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <BrowserRouter>
+      <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/competences" component={Knowledges} />
+      <Route path="/portfolio" component={Portfolio} />
+      <Route path="/contact" component={Contact} />
+      <Route component={NotFound} />
+      </Switch>
+      </BrowserRouter>
+    </>
   );
 };
 
---
+++ src/pages/Knowledges.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Knowledges = () => {
+    return (
+        <div>
+            Ceci sont mes compétences
+        </div>
+    );
+};
+
+export default Knowledges;
