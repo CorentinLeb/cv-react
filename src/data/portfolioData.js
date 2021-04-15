2937a5ec7fe25970df1f4c25385eadd6a5e05661
Corentin LE BERRE <leberre.corentin@gmail.com>
2021-04-15 14:28:14 +0200

    adding popup card with infos on portfolio

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -5,7 +5,7 @@ export const portfolioData = [
         languages: ['javascript', 'sass'],
         languagesIcons: ['fab fa-js', 'fab fa-css3-alt'],
         source: 'https://github.com/CorentinLeb/TheResto.git',
-        info: 'travail sur une maquette lors de ma formation chez Talis Business School. Utilisation de Javascript pour le carousel du header, et de SASS pour le style',
+        info: 'Travail sur une maquette lors de ma formation chez Talis Business School. Utilisation de Javascript pour le carousel du header, et de SASS pour le style',
         picture: './media/resto.jpg'
     },
 
@@ -15,7 +15,7 @@ export const portfolioData = [
         languages: ['javascript', 'sass', 'react'],
         languagesIcons: ['fab fa-js', 'fab fa-css3-alt', 'fab fa-react'],
         source: 'https://github.com/CorentinLeb/react-vitrine.git',
-        info: 'entrainement personnel pour maitriser React, création de site vitrine',
+        info: 'Entrainement personnel pour maitriser React, création de site vitrine',
         picture: './media/test.jpg'
     },
 
