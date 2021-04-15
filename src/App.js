54b536195319d5694bb87bbcaadd402b09237d58
Corentin LE BERRE <leberre.corentin@gmail.com>
2021-04-15 14:16:44 +0200

    working on the portfolio

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
-import Portfolio from './pages/Contact';
+import Portfolio from './pages/Portfolio';
 import Knowledges from './pages/Knowledges';
 import NotFound from './pages/NotFound';
 import Contact from './pages/Contact'
---
+++ src/data/portfolioData.js
@@ -0,0 +1,41 @@
+export const portfolioData = [
+    {
+        id: 1,
+        name: 'TheResto',
+        languages: ['javascript', 'sass'],
+        languagesIcons: ['fab fa-js', 'fab fa-css3-alt'],
+        source: 'https://github.com/CorentinLeb/TheResto.git',
+        info: 'travail sur une maquette lors de ma formation chez Talis Business School. Utilisation de Javascript pour le carousel du header, et de SASS pour le style',
+        picture: './media/resto.jpg'
+    },
+
+    {
+        id: 2,
+        name: 'Site Vitrine',
+        languages: ['javascript', 'sass', 'react'],
+        languagesIcons: ['fab fa-js', 'fab fa-css3-alt', 'fab fa-react'],
+        source: 'https://github.com/CorentinLeb/react-vitrine.git',
+        info: 'entrainement personnel pour maitriser React, création de site vitrine',
+        picture: './media/test.jpg'
+    },
+
+    {
+        id: 3,
+        name: 'TheResto',
+        languages: ['javascript', 'sass'],
+        languagesIcons: ['fab fa-js', 'fab fa-css3-alt'],
+        source: 'https://github.com/CorentinLeb/TheResto.git',
+        info: 'travail sur une maquette lors de ma formation chez Talis Business School. Utilisation de Javascript pour le carousel du header, et de SASS pour le style',
+        picture: './media/resto.jpg'
+    },
+
+    {
+        id: 4,
+        name: 'TheResto',
+        languages: ['javascript', 'sass'],
+        languagesIcons: ['fab fa-js', 'fab fa-css3-alt'],
+        source: 'https://github.com/CorentinLeb/TheResto.git',
+        info: 'travail sur une maquette lors de ma formation chez Talis Business School. Utilisation de Javascript pour le carousel du header, et de SASS pour le style',
+        picture: './media/resto.jpg'
+    },
+]
