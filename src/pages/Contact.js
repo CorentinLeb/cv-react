797b28474c4318667cc979013547a32f27308f70
Corentin LE BERRE <leberre.corentin@gmail.com>
2021-04-15 15:06:43 +0200

    adding the contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,13 +1,73 @@
-import React from 'react';
-import Navigation from '../components/Navigation';
+import React from "react";
+import Navigation from "../components/Navigation";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 const Contact = () => {
-    return (
-        <div>
-            <Navigation />
-            Hello depuis contact
+  return (
+    <div className="contact">
+      <Navigation />
+      <div className="contactContent">
+        <div className="header">
+          <div className="contactBox">
+            <h1>Contactez-moi</h1>
+            <ul>
+              <li>
+                <i className="fas fa-map-marker-alt"></i>
+                <span>Bordeaux</span>
+              </li>
+              <li>
+                <i className="fas fa-mobile-alt"></i>
+                <CopyToClipboard text="0620339202">
+                  <span
+                    className="clickInput"
+                    onClick={() => {
+                      alert("Téléphone copié!");
+                    }}
+                  >
+                    06 20 33 92 02
+                  </span>
+                </CopyToClipboard>
+              </li>
+              <li>
+                <i className="far fa-envelope"></i>
+                <CopyToClipboard text="leberre.corentin@gmail.com">
+                  <span
+                    className="clickInput"
+                    onClick={() => {
+                      alert("Email copié!");
+                    }}
+                  >
+                    leberre.corentin@gmail.com
+                  </span>
+                </CopyToClipboard>
+              </li>
+            </ul>
+          </div>
         </div>
-    );
+
+        <div className="socialNetwork">
+          <ul>
+            <a
+              href="https://www.linkedin.com/in/corentin-le-berre/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h4>LinkedIn</h4>
+              <i className="fab fa-linkedin"></i>
+            </a>
+            <a
+              href="https://github.com/CorentinLeb?tab=repositories"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h4>Github</h4>
+              <i className="fab fa-github"></i>
+            </a>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
