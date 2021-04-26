130ce030288c9a1fe0f63b84b0642e220fbd654f
Corentin LE BERRE <leberre.corentin@gmail.com>
2021-04-26 12:39:52 +0200

    working on back-end

--- server/server.js
+++ server/server.js
@@ -4,7 +4,8 @@ const userRoutes = require('./routes/user.routes');
 require('./config/db');
 const app = express();
 
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // routes
 app.use('/api/user', userRoutes);
